a67596abcbaf8a5c8b83615fd2ae0f953cdf7b03
unknown <j.thiagobf1992@gmail.com>
2021-12-20 14:40:46 -0300

    commit

--- post/models.py
+++ post/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', null=True)
     text = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True) # data automatica
 
 
 
