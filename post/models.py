39683325106da31b7399037880ebd5aacd1af59f
unknown <j.thiagobf1992@gmail.com>
2021-12-20 13:33:24 -0300

    adiciona os models ao projeto

--- post/models.py
+++ post/models.py
@@ -1,5 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    slug = models.SlugField(unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    title = models.CharField(max_length=200, null=False)
+    slug = models.SlugField(unique=True, null=False)
+    subtitle = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=False)
+    image = models.ImageField(upload_to='images/', null=True)
+    text = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
 
 
