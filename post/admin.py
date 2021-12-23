75081309b16d0abc5c4b845e7f1fb06b7f973a86
unknown <j.thiagobf1992@gmail.com>
2021-12-23 15:18:59 -0300

    commit

--- post/admin.py
+++ post/admin.py
@@ -7,8 +7,13 @@ from .models import Category, Post
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'slug', 'created_at']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'update_at']
+    list_filter = ['category']
 
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
