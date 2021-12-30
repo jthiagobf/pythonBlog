a7c8e024a7c80efe26a20150b5a3b1ec7f1c4cc9
unknown <j.thiagobf1992@gmail.com>
2021-12-30 13:00:38 -0300

    !!!

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from post.models import Post
 
 
 def index(request):
-    return render(request, 'index.html')
+    post_random = Post.objects.order_by('?')[:4]
+
+    context = {
+        'post_random': post_random
+    }
+    return render(request, 'index.html', context)
