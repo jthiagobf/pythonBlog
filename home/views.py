95820f27bb64feb477e4db9048df78f668f713a7
unknown <j.thiagobf1992@gmail.com>
2022-01-02 01:09:02 -0300

    commit

--- home/views.py
+++ home/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-from post.models import Post
+from post.models import Post, Category
 
 
 def index(request):
     post_random = Post.objects.order_by('?')[:4]  # adiciona fotoa aleatoriamente
+    post_latest = Post.objects.order_by('id')[:3]
+    category = Category.objects.all()
 
     context = {
-        'post_random': post_random
+        'post_random': post_random,
+        'post_latest': post_latest,
+        'category': category
+
     }
     return render(request, 'index.html', context)
