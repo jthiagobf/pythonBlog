09a6415718919310c7f16abb3404a714a63b3e1e
unknown <j.thiagobf1992@gmail.com>
2022-01-01 23:50:59 -0300

    commit

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from post.models import Post
 
 
 def index(request):
-    post_random = Post.objects.order_by('?')[:4]
+    post_random = Post.objects.order_by('?')[:4]  # adiciona fotoa aleatoriamente
 
     context = {
         'post_random': post_random
